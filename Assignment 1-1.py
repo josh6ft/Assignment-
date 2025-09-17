96c3641579a52a7fb1497c939fcaf4dd41254016
josh6ft <joshmukonyora@gmail.com>
2025-09-17 09:22:12 +0200

    Add files via upload

---
+++ Assignment 1-1.py
@@ -0,0 +1,19 @@
+# TANAKA MAHASO R221761H
+# Example of variables and constants in Python
+
+# Constant (by convention, constants are written in ALL CAPS)
+PI = 3.14159
+
+# Variables
+radius = 5
+height = 10
+
+# Calculations using variables and the constant
+circle_area = PI * (radius ** 2)
+cylinder_volume = circle_area * height
+
+# Using print() function to display values
+print("Radius of the circle:", radius)
+print("Height of the cylinder:", height)
+print("Area of the circle:", circle_area)
+print("Volume of the cylinder:", cylinder_volume)
